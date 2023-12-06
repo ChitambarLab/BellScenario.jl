ba44badb9c1a310a99e284a8dc26bd0e1c664685
Brian Doolittle <brian.d.doolittle@gmail.com>
2023-12-06 08:40:47 -0500

    adding test for local test_behavior

--- test/unit/LocalPolytope/linear_nonclassicality_witnesses.jl
+++ test/unit/LocalPolytope/linear_nonclassicality_witnesses.jl
@@ -38,12 +38,15 @@ using BellScenario
 
         chsh_vertices = LocalPolytope.vertices(chsh_scenario, "non-signaling")
 
-        pr_box_test_behavior = [1, 1, 1, 1, 1, 1, 1, 0] / 2
+        pr_box_test_behavior = [1, 1, 1, 1, 1, 1, 1, 0] / 2  # nonlocal test behavior
+        local_test_behavior = [1/2, 1/2, 1/2, 1/2, 1/4, 1/4, 1/4, 1/4]  # white noise behavior
 
-        facet_vec = LocalPolytope.linear_nonclassicality_witness(chsh_vertices, pr_box_test_behavior[:])
+        chsh_facet_vec = LocalPolytope.linear_nonclassicality_witness(chsh_vertices, pr_box_test_behavior[:])
+        local_game_vec = LocalPolytope.linear_nonclassicality_witness(chsh_vertices, local_test_behavior[:])
 
         # -2*PA(0|0) - 2*PB(0|0) + 2*PAB(00|00) + 2*PAB(01|00) + 2*PAB(10|00) - 2*PAB(11|00) <= 0
-        @test facet_vec ≈ [-2, 0, -2, 0, 2, 2, 2, -2, 0]
+        @test chsh_facet_vec ≈ [-2, 0, -2, 0, 2, 2, 2, -2, 0]
+        @test local_game_vec ≈ [0, 0, 0, 0, 0, 0, 0, 0, 0]
     end
 end
 
